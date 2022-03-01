6674016fcf16d23e57a041e6991c91601c825896
Dondre Mclenan <d.mclenan@gmail.com>
2022-03-01 16:21:49 -0500

    git add

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
+
+module.exports = router;
